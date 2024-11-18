212dda7a3d985587523471614aa31f53f7aa7d2f
vrodrigueskauan <kauanvenanciobarata@gmail.com>
2024-11-18 09:02:06 -0300

    AAAA

--- dashboard.php
+++ dashboard.php
@@ -97,39 +97,21 @@ $pedidos = $stmt->fetchAll();
         <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
         <polyline points="9 22 9 12 15 12 15 22" /></svg>
         </a>
-        <a href="" class="app-sidebar-link">
-        <svg class="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-pie-chart" viewBox="0 0 24 24">
-        <defs />
-        <path d="M21.21 15.89A10 10 0 118 2.83M22 12A10 10 0 0012 2v10z" />
-        </svg>
-        </a>
-        <a href="" class="app-sidebar-link">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar">
-        <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
-        <line x1="16" y1="2" x2="16" y2="6" />
-        <line x1="8" y1="2" x2="8" y2="6" />
-        <line x1="3" y1="10" x2="21" y2="10" /></svg>
-        </a>
-        <a href="" class="app-sidebar-link">
-        <svg class="link-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-settings" viewBox="0 0 24 24">
-        <defs />
-        <circle cx="12" cy="12" r="3" />
-        <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-2 2 2 2 0 01-2-2v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83 0 2 2 0 010-2.83l.06-.06a1.65 1.65 0 00.33-1.82 1.65 1.65 0 00-1.51-1H3a2 2 0 01-2-2 2 2 0 012-2h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 010-2.83 2 2 0 012.83 0l.06.06a1.65 1.65 0 001.82.33H9a1.65 1.65 0 001-1.51V3a2 2 0 012-2 2 2 0 012 2v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 0 2 2 0 010 2.83l-.06.06a1.65 1.65 0 00-.33 1.82V9a1.65 1.65 0 001.51 1H21a2 2 0 012 2 2 2 0 01-2 2h-.09a1.65 1.65 0 00-1.51 1z" />
-        </svg>
-        </a>
+        
+       
         </div>
         <div class="projects-section">
 
         <form method="GET" action="dashboard.php">
-        <label for="servico">Filtrar por Serviço:</label>
+        <label for="servico">Filtrar por Servico:</label>
         <select id="servico" name="servico">
             <option value="">Todos</option>
-            <option value="infraestrutura" <?php if ($servicoFiltro == 'infraestrutura') echo 'selected'; ?>>Manutenção de Infraestrutura</option>
-            <option value="software" <?php if ($servicoFiltro == 'software') echo 'selected'; ?>>Manutenção de Software</option>
+            <option value="infraestrutura" <?php if ($servicoFiltro == 'infraestrutura') echo 'selected'; ?>>Manutencao de Infraestrutura</option>
+            <option value="software" <?php if ($servicoFiltro == 'software') echo 'selected'; ?>>Manutencao de Software</option>
             <option value="consultoria" <?php if ($servicoFiltro == 'consultoria') echo 'selected'; ?>>Consultoria</option>
         </select>
         
-        <label for="id_usuario">Filtrar por ID do Usuário:</label>
+        <label for="id_usuario">Filtrar por ID do Usuario:</label>
         <input type="text" id="id_usuario" name="id_usuario" value="<?php echo htmlspecialchars($idUsuarioFiltro); ?>">
 
         <button type="submit">Buscar</button>
@@ -141,8 +123,8 @@ $pedidos = $stmt->fetchAll();
                     <th>Selecionar</th>
                     <th>ID Pedido</th>
                     <th>ID Usuário</th>
-                    <th>Serviço</th>
-                    <th>Descrição</th>
+                    <th>Servico</th>
+                    <th>Descricao</th>
                     <th>Data do Pedido</th>
                 </tr>
             </thead>
@@ -163,97 +145,7 @@ $pedidos = $stmt->fetchAll();
         <button type="submit">Excluir Pedidos Selecionados</button>
     </form>
         <div class="projects-section-header">
-        <p>mensagem dos clientes</p>
-        </div>
-        <div class="messages">
-        <div class="message-box">
-        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile image">
-        <div class="message-content">
-        <div class="message-header">
-        <div class="name">Stephanie</div>
-        <div class="star-checkbox">
-        <input type="checkbox" id="star-1">
-        <label for="star-1">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-        </label>
-        </div>
-        </div>
-        <p class="message-line">
-        gostei muito, programaram meu pc para eu lembrar de lavar a louca
-        </p>
-        <p class="message-line time">
-        11/11
-        </p>
-        </div>
-        </div>
-        <div class="message-box">
-        <img src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="profile image">
-        <div class="message-content">
-        <div class="message-header">
-        <div class="name">adolfo</div>
-        <div class="star-checkbox">
-        <input type="checkbox" id="star-2">
-        <label for="star-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-        </label>
-        </div>
-        </div>
-        <p class="message-line">
-        consegue me dizer se posso avaliar?
-        </p>
-        <p class="message-line time">
-        08/02
-        </p>
-        </div>
-        </div>
-        <div class="message-box">
-        <img src="https://images.unsplash.com/photo-1543965170-4c01a586684e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fG1hbnxlbnwwfDB8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="profile image">
-        <div class="message-content">
-        <div class="message-header">
-        <div class="name">alexey</div>
-        <div class="star-checkbox">
-        <input type="checkbox" id="star-3">
-        <label for="star-3">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-        </label>
-        </div>
-        </div>
-        <p class="message-line">
-        preciso de reparo!
-        </p>
-        <p class="message-line time">
-        02/04
-        </p>
-        </div>
-        </div>
-        <div class="message-box">
-        <img src="https://images.unsplash.com/photo-1533993192821-2cce3a8267d1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWFuJTIwbW9kZXJufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="profile image">
-        <div class="message-content">
-        <div class="message-header">
-        <div class="name">Jessica</div>
-        <div class="star-checkbox">
-        <input type="checkbox" id="star-4">
-        <label for="star-4">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-        <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
-        </label>
-        </div>
-        </div>
-        <p class="message-line">
-       muito bom! viva a byte amigo
-        </p>
-        <p class="message-line time">
-        22/03
-        </p>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
+        
 
         <script src="./js/dash.js"></script>
 
