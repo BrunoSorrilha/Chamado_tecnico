38b9361d8138eabc8ec76aa6de2d080b087c9cc9
joaomorangoni <joaopedromorangoni@gmail.com>
2024-11-21 12:34:17 -0300

    banco

--- chamado_tecnico.sql
+++ chamado_tecnico.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 08/11/2024 às 18:51
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 21-Nov-2024 às 16:33
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,7 +26,24 @@ USE `chamado_tecnico`;
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `pedidos`
+-- Estrutura da tabela `historico`
+--
+
+CREATE TABLE `historico` (
+  `id_hist` int(11) NOT NULL,
+  `id_pedido` int(11) DEFAULT NULL,
+  `servico` varchar(50) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `data_pedido` date DEFAULT NULL,
+  `Nome` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `situacao` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedidos`
 --
 
 CREATE TABLE `pedidos` (
@@ -34,20 +51,29 @@ CREATE TABLE `pedidos` (
   `ID_usuario` int(11) DEFAULT NULL,
   `servico` varchar(50) NOT NULL,
   `descricao` text DEFAULT NULL,
-  `data_pedido` timestamp NOT NULL DEFAULT current_timestamp()
+  `data_pedido` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ID_tecnico` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `pedidos`
---
-
-INSERT INTO `pedidos` (`id`, `ID_usuario`, `servico`, `descricao`, `data_pedido`) VALUES
-(3, 5, 'infraestrutura', 'deu problema aqui', '2024-11-08 17:27:18');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuario`
+-- Estrutura da tabela `tecnico`
+--
+
+CREATE TABLE `tecnico` (
+  `ID_tecnico` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `especialidade` varchar(255) DEFAULT NULL,
+  `telefone` int(12) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -60,58 +86,81 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuario`
---
-
-INSERT INTO `usuario` (`ID_usuario`, `Nome`, `Email`, `telefone`, `Administrator`, `senha`) VALUES
-(1, 'kauan', 'kauan@email.com', 2147483647, NULL, '$2y$10$06y8.Bw/e1lr3QGH2W62kOM119gIPMUfneUo4DTQ8Tz56VNSRU8Tq'),
-(2, 'vinicius', 'vcin@gmail.com', 2147483647, NULL, '$2y$10$ot1Byp5CGP.ZXfATByW1CejQ5xcbzdWKRQy3ujZ6i3Ve1FRNgssqi'),
-(5, 'Joao Pedro', 'joao@email.com', 953022544, b'1', '$2y$10$XQULcT6c2V7WiyTu97VqfukqSDAmvoXwxGOcKT4TIeHzQtQP58Bju'),
-(6, 'Anderson Vanin', 'anderson@email.com', 999999999, b'1', '$2y$10$e7KO9Uxxj/Vj4LCsZxr2beI/3pbSVRmfLtENI.VxfdJs8Ymc5HKoK');
-
---
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `pedidos`
+-- Índices para tabela `historico`
+--
+ALTER TABLE `historico`
+  ADD PRIMARY KEY (`id_hist`),
+  ADD KEY `id_pedido` (`id_pedido`);
+
+--
+-- Índices para tabela `pedidos`
 --
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `ID_usuario` (`ID_usuario`);
+  ADD KEY `ID_usuario` (`ID_usuario`),
+  ADD KEY `ID_tecnico` (`ID_tecnico`);
 
 --
--- Índices de tabela `usuario`
+-- Índices para tabela `tecnico`
+--
+ALTER TABLE `tecnico`
+  ADD PRIMARY KEY (`ID_tecnico`);
+
+--
+-- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`ID_usuario`),
   ADD UNIQUE KEY `Email` (`Email`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `historico`
+--
+ALTER TABLE `historico`
+  MODIFY `id_hist` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `tecnico`
+--
+ALTER TABLE `tecnico`
+  MODIFY `ID_tecnico` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `ID_usuario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID_usuario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `pedidos`
+-- Limitadores para a tabela `historico`
+--
+ALTER TABLE `historico`
+  ADD CONSTRAINT `historico_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id`);
+
+--
+-- Limitadores para a tabela `pedidos`
 --
 ALTER TABLE `pedidos`
-  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`ID_usuario`) REFERENCES `usuario` (`ID_usuario`);
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`ID_usuario`) REFERENCES `usuario` (`ID_usuario`),
+  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`ID_tecnico`) REFERENCES `tecnico` (`ID_tecnico`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
